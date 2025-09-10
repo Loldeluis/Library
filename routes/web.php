19c161c48b135cbb37954f6ea94fc6903eb05647
Loldeluis <luismaldonadourib@gmail.com>
2025-09-10 11:58:32 -0500

    Gestion de roles

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\PerfilController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookRegistrationController;
@@ -22,27 +21,36 @@ Route::get('/', function () {
         : redirect()->route('login');
 });
 
-// 2. Rutas públicas (login, register, etc.) → Breeze las carga desde auth.php
-require __DIR__.'/auth.php';
+    // 2. Rutas públicas (login, register, etc.) → Breeze las carga desde auth.php
+    require __DIR__.'/auth.php';
 
-// 3. Rutas protegidas por autenticación
-Route::middleware(['auth'])->group(function () {
-    // Dashboard oficial
+    // 3. Rutas protegidas por autenticación
+    Route::middleware(['auth'])->group(function () {
+    
+    // Dashboard para estudiantes
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    //Dashboard para administrador
+        Route::middleware('rol:administrador')->get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+
+
 
        // Perfil del usuario
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    //otra ruta admin (ejemplo)
+    Route::get('/admin', [AdminController::class, 'index'])->middleware('rol:administrador');
 
-Route::get('/admin', [AdminController::class, 'index'])->middleware('rol:administrador');
+    // RUTAS PARA LIBROS
 
-// RUTAS PARA LIBROS
-
-Route::get('books/create', [BookRegistrationController::class, 'create'])->name('books.create');
-Route::post('books',       [BookRegistrationController::class, 'store'])->name('books.store');
+    Route::get('books/create', [BookRegistrationController::class, 'create'])->name('books.create');
+    Route::post('books',       [BookRegistrationController::class, 'store'])->name('books.store');
 
 
 });
